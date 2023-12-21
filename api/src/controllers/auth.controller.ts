81a363654193e091c00a335b29a02709e0370788
zlskey <zielski@pm.me>
2023-12-21 18:28:46 +0100

    Update expiration time of jwt cookie

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -9,47 +9,41 @@ import { getJwtFromHeader } from 'src/utils/get-jwt-from-header'
 import { jwtUtils } from 'src/utils'
 import { userService } from 'src/services'
 
-const getMaxAge = remember => {
-  const oneDay = 1000 * 60 * 60 * 24
-
-  return remember ? oneDay * 365 : oneDay * 3
-}
-
 const domain = process.env.MAIN_DOMAIN || 'localhost'
 
-const getCookieOptions = remember => ({
-  maxAge: getMaxAge(remember),
+const maxAge = 1000 * 60 * 60 * 24 * 3
+
+const cookieOptions = {
+  maxAge,
   httpOnly: true,
   secure: true,
   domain,
-})
+}
 
 export const signup: RequestHandler = async (req, res, next) => {
-  const { username, password, remember } = req.body
+  const { username, password } = req.body
 
   await validateAndThrow(userValidationSchema, username, password)
 
-  const maxAge = getMaxAge(remember) / 1000
   const user = await userService.create(username, password)
   const token = jwtUtils.createToken(user._id, maxAge)
 
-  res.cookie('jwt', token, getCookieOptions(remember)).json({
+  res.cookie('jwt', token, cookieOptions).json({
     user: user.removePassword(),
     token,
   })
 }
 
 export const login: RequestHandler = async (req, res, next) => {
-  const { username, password, remember } = req.body
+  const { username, password } = req.body
 
   const user = await userService.getByUsername(username)
 
   await user.validatePassword(password)
 
-  const maxAge = getMaxAge(remember) / 1000
   const token = jwtUtils.createToken(user._id, maxAge)
 
-  res.cookie('jwt', token, getCookieOptions(remember)).json({
+  res.cookie('jwt', token, cookieOptions).json({
     user: user.removePassword(),
     token,
   })
@@ -73,9 +67,9 @@ export const whoami: RequestHandler = async (req, res, next) => {
         token: jwt,
       })
     )
-    .catch(() => res.clearCookie('jwt', getCookieOptions(false)).json(null))
+    .catch(() => res.clearCookie('jwt', cookieOptions).json(null))
 }
 
 export const logout: RequestHandler = async (req, res, next) => {
-  res.clearCookie('jwt', getCookieOptions(false)).status(200).json({})
+  res.clearCookie('jwt', cookieOptions).status(200).json({})
 }
