2bf99e74c1a4a58004783c73c995d58c93f268d9
zlskey <zielski@pm.me>
2024-01-05 21:18:25 +0100

    [temp] remove discount functionality

--- api/src/utils/get-prompt.ts
+++ api/src/utils/get-prompt.ts
@@ -1,6 +1,5 @@
 export const getPrompt = (receipt: string) => `
     From the receipt below, extract a list of the products purchased with their price, the amount for which the receipt was issued and the title of the operation should include the abbreviated name of the shop (without company information etc.) and the date of the operation.
-    Sometimes there are discounts for some products in a receipt. It is usually written under the product line. Try to include this value in 'discount' property.
     Since receipt is ridden from an image of variety of resolutions, name of the products won't always make sense. If you spot a typo in the \`name\` property, try to rewrite it as you understand current product. 
     Your response should be in (json) format:
     {
@@ -11,7 +10,6 @@ export const getPrompt = (receipt: string) => `
                 name: string,
                 price: number, // remember to get price for single product, not the sum
                 count: number,
-                discount: number
             }
         ]
     }
